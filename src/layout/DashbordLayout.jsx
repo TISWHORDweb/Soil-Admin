4f92b57fab89a6427decc181be082438d0ed3362
Idah Abubakar Isah <idahabubakar49@gmail.com>
2025-02-18 12:51:31 +0100

    More updates

--- src/layout/DashbordLayout.jsx
+++ src/layout/DashbordLayout.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Button } from "antd";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import global from "../assets/global.png";
 import notification from "../assets/notification-bing.png";
@@ -28,7 +28,7 @@ function getItem(label, key, icon) {
 
 const items = [
   getItem("Dashboard", "/dashboard", <MdDashboardCustomize className="w-4" />),
-  getItem("Farmers", "/famers", <HiMiniUsers className="w-4" />),
+  getItem("Farmers", "/farmers", <HiMiniUsers className="w-4" />),
   getItem("Lands", "/lands", <RiCouponLine className="w-4" />),
   getItem("Agents", "/agents", <CiUser className="w-4" />),
   getItem("Test Requests", "/requests", <CiUser className="w-4" />),
@@ -40,7 +40,6 @@ const items = [
 ];
 
 const DashboardLayout = () => {
- 
   const { state, dispatch } = useUserContext();
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
@@ -50,7 +49,7 @@ const DashboardLayout = () => {
   const handleResize = () => {
     setCollapsed(window.innerWidth < 1020);
   };
-  
+
   useEffect(() => {
     window.addEventListener("resize", handleResize);
     handleResize();
@@ -58,21 +57,20 @@ const DashboardLayout = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  // Check for the user data in localStorage on initial load
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
-      dispatch({ type: "SET_USER", payload: JSON.parse(storedUser) }); // Assume you have a SET_USER action
+      dispatch({ type: "SET_USER", payload: JSON.parse(storedUser) });
     } else {
-      navigate("/login");  // Redirect to login page if no user is found
+      navigate("/login");
     }
   }, [dispatch, navigate]);
 
-  const adminUsername = state.user ? state.user.user.profile.firstName +" "+ state.user.user.profile.lastName   : "";
+  const adminUsername = state.user ? state.user.user.profile.firstName + " " + state.user.user.profile.lastName : "";
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
-    localStorage.removeItem("user"); // Clear persisted data
+    localStorage.removeItem("user");
     navigate("/login");
   };
 
@@ -87,7 +85,7 @@ const DashboardLayout = () => {
         style={{ zIndex: 1000 }}
       >
         <div className="p-4 flex items-center text-xl font-bold">
-          <span className="text-white text-2xl">Agric Portal</span>
+          <span className={`text-white ${collapsed ? "text-[20px]" : "text-2xl"}`}>Agric Portal</span>
         </div>
         <Menu
           theme="dark"
@@ -100,8 +98,8 @@ const DashboardLayout = () => {
       </Sider>
 
       <Layout style={{ marginLeft: collapsed ? 80 : 250 }}>
-        <Header className="bg-white fixed top-0 left-0 right-0 flex items-center w-full z-50 pl-[var(--sider-width)]">
-          <div className="flex justify-end items-center w-full">
+        <Header className="bg-white fixed  flex items-center w-full z-50 pl-[var(--sider-width)]">
+          <div className="flex items-center justify-end max-w-[75%] w-full">
             <div className="flex gap-3">
               <div className="icon-container">
                 <img src={global} alt="Global" className="icon" />
@@ -114,15 +112,12 @@ const DashboardLayout = () => {
               <MenuButton className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 ml-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                 <div>
                   {adminUsername ? (
-                    <h3>Welcome, {adminUsername}!</h3>
+                    <h3 className={`text-sm ${collapsed ? "text-xs" : ""}`}>Welcome, {adminUsername}!</h3>
                   ) : (
-                    <h3>Welcome, Admin!</h3>
+                    <h3 className={`text-sm ${collapsed ? "text-xs" : ""}`}>Welcome, Admin!</h3>
                   )}
                 </div>
-                <ChevronDownIcon
-                  className="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
+                <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
               </MenuButton>
               <MenuItems className="absolute z-10 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
                 <div>
@@ -130,11 +125,8 @@ const DashboardLayout = () => {
                     {({ active }) => (
                       <button
                         type="button"
-                        className={`block px-4 py-2 text-left text-sm ${
-                          active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                        }`}
+                        className={`block px-4 py-2 text-left text-sm ${active ? "bg-gray-100 text-gray-900" : "text-gray-700"}`}
                         onClick={handleLogout}
-                        tabIndex={0}
                       >
                         Sign out
                       </button>
--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -1,39 +1,53 @@
-
-
 const ChangePassword = () => {
-
-
   return (
-    <>
-      <div className="flex items-center justify-center">
-        <div className="bg-white shadow-md rounded-md w-full max-w-[30vw] h-[320px] mx-auto mt-20 p-4 relative">
-          <form action=" ">
-            <div className="">
-              <label className="block text-sm font-medium py-2">Old password</label>
-              <input
-                type="text"
-                className="bg-[#f6f6f6] rounded-xl outline-none pr-52 pl-5 py-3"
-                placeholder="Old Password" />
-            </div>
-            <div className="">
-              <label className="block text-sm font-medium py-2">New password</label>
-              <input
-                type="text"
-                className="bg-[#f6f6f6] rounded-xl outline-none pr-52 pl-5 py-3"
-                placeholder="New Password" />
-            </div>
-            <div className="">
-              <label className="block text-sm font-medium py-2">Confirm new password</label>
-              <input
-                type="text"
-                className="bg-[#f6f6f6] rounded-xl outline-none pr-52 pl-5 py-3"
-                placeholder="Confirm new password" />
-            </div>
-            <button className="bg-[#16a349]  text-white rounded-full px-7 py-2 absolute right-0 mt-4 m-5">Submit</button>
-          </form>
-        </div>
+    <div className="flex items-center justify-center min-h-screen px-4">
+      <div className="bg-white shadow-md rounded-md w-full max-w-[400px] sm:max-w-[500px] h-auto mx-auto mt-10 p-6 sm:p-8">
+        <form>
+          {/* Old Password */}
+          <div className="mb-4">
+            <label className="block text-sm sm:text-base font-medium py-2">
+              Old Password
+            </label>
+            <input
+              type="password"
+              className="bg-[#f6f6f6] rounded-xl outline-none w-full pl-3 py-3 text-base sm:text-lg placeholder:text-sm sm:placeholder:text-base"
+              placeholder="Old Password"
+            />
+          </div>
+
+          {/* New Password */}
+          <div className="mb-4">
+            <label className="block text-sm sm:text-base font-medium py-2">
+              New Password
+            </label>
+            <input
+              type="password"
+              className="bg-[#f6f6f6] rounded-xl outline-none w-full pl-3 py-3 text-base sm:text-lg placeholder:text-sm sm:placeholder:text-base"
+              placeholder="New Password"
+            />
+          </div>
+
+          {/* Confirm New Password */}
+          <div className="mb-6">
+            <label className="block text-sm sm:text-base font-medium py-2">
+              Confirm New Password
+            </label>
+            <input
+              type="password"
+              className="bg-[#f6f6f6] rounded-xl outline-none w-full pl-3 py-3 text-base sm:text-lg placeholder:text-sm sm:placeholder:text-base"
+              placeholder="Confirm New Password"
+            />
+          </div>
+
+          {/* Submit Button */}
+          <div className="w-full flex justify-center">
+            <button className="bg-[#16a349] text-white font-semibold rounded-full px-7 py-2 text-base sm:text-lg w-full sm:w-auto">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
